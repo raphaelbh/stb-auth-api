946ec2f41271d6d2bcd3c4869699bd5191e49d88
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-04-14 11:34:54 -0300

    feat: fix flake 8 issues

--- application/login/index.py
+++ application/login/index.py
@@ -1,12 +1,13 @@
 import json
 
+
 def lambda_handler(event, context):
 
     body = json.dumps({
-        "access_token":"MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3",
-        "token_type":"Bearer",
-        "expires_in":3600,
-        "refresh_token":"IwOGYzYTlmM2YxOTQ5MGE3YmNmMDFkNTVk"
+        "access_token": "MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3",
+        "token_type": "Bearer",
+        "expires_in": 3600,
+        "refresh_token": "IwOGYzYTlmM2YxOTQ5MGE3YmNmMDFkNTVk"
     })
 
     return {
