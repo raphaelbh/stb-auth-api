48583414290a86b0bf989a60b8b92ab635aff4ac
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-05-03 14:42:45 -0300

    feat: signup feature
    create base content and unit tests

--- application/signup/app.py
+++ application/signup/app.py
@@ -1,7 +1,5 @@
 def lambda_handler(event, context):
+
     return {
-        "statusCode": 201,
-        "headers": {
-            "Content-Type": "application/json"
-        }
+        "statusCode": 201
     }
