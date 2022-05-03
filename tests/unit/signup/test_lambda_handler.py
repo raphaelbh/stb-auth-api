df02207b2bc4bae0573737db860404b4b49c5cf7
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-05-03 15:54:10 -0300

    feat: fix flake8 issue

--- tests/unit/signup/test_lambda_handler.py
+++ tests/unit/signup/test_lambda_handler.py
@@ -23,6 +23,7 @@ def apigw_event():
         "httpMethod": "POST"
     }
 
+
 def test_lambda_handler_success(apigw_event):
     response = app.lambda_handler(apigw_event, "")
     assert response["statusCode"] == 201
