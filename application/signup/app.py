fc3f6a1bb9d83fbede4e78c56a29c09122a2f4c4
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-05-05 17:08:54 -0300

    feat: signup feature
    sign up user and confirm

--- application/signup/app.py
+++ application/signup/app.py
@@ -1,18 +1,47 @@
 import os
+import sys
 import json
+import boto3
 
+
+AWS_REGION = os.environ.get("AWS_REGION")
+USER_POOL_ID = os.environ.get("USER_POOL_ID")
+USER_POOL_CLIENT_ID = os.environ.get("USER_POOL_CLIENT_ID")
+
+cognito_idp = boto3.client('cognito-idp', region_name=AWS_REGION)
 
 def lambda_handler(event, context):
-    body = json.dumps({
-        "REGION": os.environ.get("REGION"),
-        "AWS_REGION": os.environ.get("AWS_REGION"),
-        "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION"),
-        "USER_POOL_ID": os.environ.get("USER_POOL_ID"),
-        "USER_POOL_CLIENT_ID": os.environ.get("USER_POOL_CLIENT_ID")
-    })
+    user = json.loads(event['body'])
+    try:
+        _sign_up(user)
+        _admin_confirm_sign_up(user)
+        return {
+            "statusCode": 201
+        }
+    except:
+        e = sys.exc_info()[1]
+        body = json.dumps({
+            "message": "Sign up error",
+            "error": e.args[0]
+        })
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": body
+        }
 
-    return {
-        "statusCode": 201,
-        "body": body
-    }
+def _sign_up(user):
+    cognito_idp.sign_up(
+        ClientId = USER_POOL_CLIENT_ID,
+        Username = user['username'],
+        Password = user['password'],
+        UserAttributes = user['attributes']
+    )
 
+def _admin_confirm_sign_up(user):
+    cognito_idp.admin_confirm_sign_up(
+        UserPoolId = USER_POOL_ID,
+        Username = user['username']
+    )
