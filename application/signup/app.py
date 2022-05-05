ebf313c62e074960d652a6f39624a34a129c4e72
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-05-05 16:35:39 -0300

    feat: add env variables to lambda signup

--- application/signup/app.py
+++ application/signup/app.py
@@ -1,5 +1,16 @@
+import os
+import json
+
+
 def lambda_handler(event, context):
+    body = json.dumps({
+        "AWS_REGION": os.environ.get("AWS_REGION"),
+        "USER_POOL_ID": os.environ.get("USER_POOL_ID"),
+        "USER_POOL_CLIENT_ID": os.environ.get("USER_POOL_CLIENT_ID")
+    })
 
     return {
-        "statusCode": 201
+        "statusCode": 201,
+        "body": body
     }
+
